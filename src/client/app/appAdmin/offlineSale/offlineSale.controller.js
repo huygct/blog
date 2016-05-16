cc98e46b8e096add8ceab3c86b08b10a9473c9cf
Nghi TMA <thuynghi@tma.com.vn>
2016-05-16 18:55:54 +0700

    update offline sale

--- src/client/app/appAdmin/offlineSale/offlineSale.controller.js
+++ src/client/app/appAdmin/offlineSale/offlineSale.controller.js
@@ -8,24 +8,54 @@
     .module('app.admin.setting')
     .controller('OfflineSaleController', OfflineSaleController);
 
-  OfflineSaleController.$inject = ['$rootScope', '$scope', 'offlineSaleService'];
-  function OfflineSaleController($rootScope, $scope, offlineSaleService) {
+  OfflineSaleController.$inject = ['$rootScope', '$scope', 'productManagerService', 'offlineSaleService'];
+  function OfflineSaleController($rootScope, $scope, productManagerService, offlineSaleService) {
     $rootScope.nameApp = 'Offline Sale';
 
     var vm = this;
 
     vm.cache = offlineSaleService.cache;
+    vm.products = [];
 
-    $scope.vegetables = ['Corn' ,'Onions' ,'Kale' ,'Arugula' ,'Peas', 'Zucchini'];
-    $scope.searchTerm;
-    $scope.clearSearchTerm = function() {
-      $scope.searchTerm = '';
+    vm.searchTerm = '';
+    vm.clearSearchTerm = function() {
+      vm.searchTerm = '';
     };
     // The md-select directive eats keydown events for some quick select
     // logic. Since we have a search input here, we don't need that logic.
 //    $element.find('input').on('keydown', function(ev) {
 //      ev.stopPropagation();
 //    });
+
+    function getProducts() {
+      productManagerService.api.getAllProducts()
+        .then(function (response) {
+          vm.products = response.data;
+        })
+        .catch(function () {
+          vm.products = [];
+        })
+    }
+
+    function getOrderOfflineSale() {
+
+    }
+
+    function getSumMoney(selectedProducts, otherCost) {
+      var sum = 0;
+      var numberBuy = 1;
+      var other = otherCost || 0;
+      other = parseFloat(other);
+      _.forEach(selectedProducts, function (p) {
+        numberBuy = p.quantityWillBuy || 1;
+        if(p.sale && p.sale > 0) {
+          sum+=(p.sale * numberBuy);
+        } else {
+          sum+=(p.price * numberBuy);
+        }
+      });
+      return (sum + other).formatMoney(0, '.', ',');
+    }
 
     vm.backToListOrderView = function() {
       vm.cache.currentView = offlineSaleService.getView.main;
@@ -41,6 +71,18 @@
       vm.cache.currentView = offlineSaleService.getView.edit;
       vm.cache.currentOfflineSale = currentOfflineSale;
     };
+
+    vm.addOfflineSale = function addOfflineSale(newOfflineSale) {
+      console.log('-- ', newOfflineSale);
+    };
+
+    vm.updateOfflineSale = function updateOfflineSale(upd) {
+
+    };
+
+    //------------------------------------------------
+    vm.getProducts = getProducts;
+    vm.getSumMoney = getSumMoney;
 
   }
 
---
+++ src/client/myComponents/numberSpinner/numberSpinner.js
@@ -0,0 +1,83 @@
+/**
+ * Created by thuynghi on 16/05/2016.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('my-compoents')
+    .component('numberSpinner', {
+      bindings: {
+        value: '=',
+        config: '<'
+      },
+      template:  '<div class=\"number-spinner-component\"><div class=\"input-group number-spinner\">' +
+                    '<span class=\"input-group-btn data-dwn\">' +
+                        '<button class=\"btn btn-default btn-info\" ng-click=\"$ctrl.minusValue()\" ng-disabled=\"$ctrl.disableMinus\">' +
+                          '<span class=\"glyphicon glyphicon-minus\">' +
+                          '</span>' +
+                        '</button>' +
+                    '</span>' +
+                    '<input type=\"text\" class=\"form-control text-center\" ng-model=\"$ctrl.value\" min=\"$ctrl.config.min\" max=\"$ctrl.config.max\">' +
+                    '<span class=\"input-group-btn data-up\">' +
+                        '<button class=\"btn btn-default btn-info\" ng-click=\"$ctrl.plusValue()\" ng-disabled=\"$ctrl.disablePlus\">' +
+                          '<span class=\"glyphicon glyphicon-plus\">' +
+                          '</span>' +
+                        '</button>' +
+                    '</span>' +
+                  '</div></div>',
+
+      controller: ['$scope', function ($scope) {
+        var $ctrl = this;
+
+        if(!$ctrl.value) {
+          $ctrl.value = 1;
+        }
+
+        var defaultConfig = {
+          min: 0,
+          max: 999
+        };
+
+        if($ctrl.config) {
+          $ctrl.config = angular.extend($ctrl.config, defaultConfig);
+          if($ctrl.value > $ctrl.config.max) {
+            $ctrl.value = $ctrl.config.max;
+            $ctrl.disablePlus = true;
+          }
+          if($ctrl.value < $ctrl.config.min) {
+            $ctrl.value = $ctrl.config.min;
+            $ctrl.disableMinus = true;
+          }
+        }
+
+        function checkDisableButton(value) {
+          if(value < $ctrl.config.min) {
+            $ctrl.disableMinus = true;
+            $ctrl.disablePlus = false;
+          } else {
+            if(value > $ctrl.config.max) {
+              $ctrl.disableMinus = false;
+              $ctrl.disablePlus = true;
+            } else {
+              $ctrl.disableMinus = false;
+              $ctrl.disablePlus = false;
+              $ctrl.value = value;
+            }
+          }
+        }
+
+        $ctrl.plusValue = function plusValue() {
+          var newValue = $ctrl.value + 1;
+          checkDisableButton(newValue);
+        };
+
+        $ctrl.minusValue = function minusValue() {
+          var newValue = $ctrl.value - 1;
+          checkDisableButton(newValue);
+        };
+
+      }]
+    })
+
+})();
