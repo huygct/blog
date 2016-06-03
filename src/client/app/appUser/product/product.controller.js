9b64d58d1c3d51098351efb4ef8e0eeaccf9da45
Nghi TMA <thuynghi@tma.com.vn>
2016-06-03 18:46:32 +0700

    resolve product

--- src/client/app/appUser/product/product.controller.js
+++ src/client/app/appUser/product/product.controller.js
@@ -8,10 +8,10 @@
     .module('app.user.product')
     .controller('ProductController', ProductController);
 
-  ProductController.$inject = ['$rootScope', '$location', 'productManagerService', 'productService',
+  ProductController.$inject = ['$rootScope', 'productsByCategoryId', 'currentProduct', 'productService',
     '$stateParams', 'coreService'];
   /* @ngInject */
-  function ProductController($rootScope, $location, productManagerService, productService,
+  function ProductController($rootScope, productsByCategoryId, currentProduct, productService,
                              $stateParams, coreService) {
     var vm = this;
 
@@ -19,54 +19,23 @@
     var currentCategoryId = $stateParams.categoryId;
 
     vm.cache = productService.cache;
-    vm.productsByCategoryId = [];
+    vm.productsByCategoryId = productsByCategoryId;
+    vm.currentProduct = currentProduct;
 
-    function loadProductById(productId) {
-      productManagerService.api.getProductById(productId)
-        .then(function (response) {
-          vm.currentProduct = response.data;
-          vm.currentProduct.quantityWillBuy = 1;
-          vm.cache.status = true;
-          /**
-           * Use to put to meta for facebook post link image
-           * @type {{url: *, description: *, siteName: *, image: string}}
-           */
-          $rootScope.currentProduct = {
-            url: $location.absUrl(),
-            description: vm.currentProduct.description,
-            siteName: vm.currentProduct.name,
-            image: $location.protocol() + '://' + location.host + '/' + vm.currentProduct.imageUrl
-          };
-        })
-        .catch(function () {
-          vm.cache.status = false;
-        });
-    }
 
-    function getProductByCategoryId(categoryId) {
-      productManagerService.api.getProductByCategoryId(categoryId)
-        .then(function (response) {
-          vm.productsByCategoryId = _.filter(response.data, function(product) {
-                                        return product.id !== currentProductId;
-                                    });
-          vm.cache.status = true;
-        })
-        .catch(function () {
-          vm.productsByCategoryId = [];
-          vm.cache.status = false;
-        });
-    }
+
+
 
 
     vm.openYourCard = coreService.openYourCard;
 
 
-    function activate() {
-      loadProductById(currentProductId);
-      getProductByCategoryId(currentCategoryId);
+    //function activate() {
+    //  loadProductById(currentProductId);
+    //  getProductByCategoryId(currentCategoryId);
       //logger.info('Activated Product View');
-    }
+    //}
 
-    activate();
+    //activate();
   }
 })();
--- src/client/app/appUser/product/product.route.js
+++ src/client/app/appUser/product/product.route.js
@@ -15,6 +15,45 @@
     routerHelper.configureStates(getStates());
   }
 
+  loadProductById.$inject = ['$rootScope', '$location', 'productManagerService', '$stateParams'];
+  function loadProductById($rootScope, $location, productManagerService, $stateParams) {
+    var productId = $stateParams.productId;
+
+    productManagerService.api.getProductById(productId)
+      .then(function (response) {
+        var currentProduct = response.data;
+        currentProduct.quantityWillBuy = 1;
+        /**
+         * Use to put to meta for facebook post link image
+         * @type {{url: *, description: *, siteName: *, image: string}}
+         */
+        $rootScope.currentProduct = {
+          url: $location.absUrl(),
+          description: currentProduct.description,
+          siteName: currentProduct.name,
+          image: $location.protocol() + '://' + location.host + '/' + currentProduct.imageUrl
+        };
+
+        return currentProduct;
+      });
+  }
+
+  loadProductById.$inject = ['productManagerService', '$stateParams'];
+  function getProductByCategoryId(productManagerService, $stateParams) {
+    var currentProductId = $stateParams.productId;
+    var categoryId = $stateParams.categoryId;
+
+    productManagerService.api.getProductByCategoryId(categoryId)
+      .then(function (response) {
+        return _.filter(response.data, function(product) {
+          return product.id !== currentProductId;
+        });
+      })
+      .catch(function () {
+        return [];
+      });
+  }
+
   /**
    * Default productId if not login: 95237041b02096bbdb38980f727e33c3local
    */
@@ -27,7 +66,11 @@
           templateUrl: 'app/appUser/product/product.html',
           controller: 'ProductController',
           controllerAs: 'vm',
-          title: 'Product'
+          title: 'Product',
+          resolve: {
+            currentProduct: loadProductById,
+            productsByCategoryId: getProductByCategoryId
+          }
         }
       }
     ];
